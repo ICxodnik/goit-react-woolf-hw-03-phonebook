4bf3d8c2a1a1450db8e7e4b75ed63de35ef95293
Natalia <icxodnik@gmail.com>
2024-04-28 16:35:55 -0400

    extract Form

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export default class Form extends Component {
+  static propTypes = { onSubmit: PropTypes.func };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const user = {
+      name: form.elements.name.value,
+      number: form.elements.number.value,
+      id: nanoid(),
+    };
+    this.props.onSubmit(user);
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+        <button type="submit">Add Contact</button>
+      </form>
+    );
+  }
+}
--- src/components/PhoneBook/index.jsx
+++ src/components/PhoneBook/index.jsx
@@ -1,25 +1,16 @@
 import css from 'components/PhoneBook/index.module.css';
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
 import Filter from 'components/Filter';
 import ContactList from 'components/ContactList';
+import Form from 'components/Form';
 
 export default class PhoneBook extends Component {
-  //   static propTypes = {second: third}
   state = {
     contacts: [],
     filter: '',
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    const user = {
-      name: form.elements.name.value,
-      number: form.elements.number.value,
-      id: nanoid(),
-    };
+  handleSubmit = user => {
     this.setState(prevState => {
       return {
         contacts: [...prevState.contacts, user],
@@ -44,23 +35,7 @@ export default class PhoneBook extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-          <button type="submit">Add Contact</button>
-        </form>
+        <Form onSubmit={this.handleSubmit} />
         <Filter onChange={this.handleFilter} />
         <ContactList contacts={this.getFilteredData()} />
       </div>
