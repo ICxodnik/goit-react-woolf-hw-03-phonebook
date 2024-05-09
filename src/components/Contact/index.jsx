ae7127752b1d8cea2b2dd8f18b7c2a3039ad9dbd
Natalia <icxodnik@gmail.com>
2024-05-09 14:02:09 +0200

    make functional components

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,21 +1,19 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import css from 'components/Contact/index.module.css';
 
-export default class Contact extends Component {
-  static propTypes = {
-    data: PropTypes.shape({
-      name: PropTypes.string,
-      id: PropTypes.string,
-    }),
-  };
-
-  render() {
-    return (
-      <li className={css.contactData}>
-        <span className={css.name}>{this.props.data.name}</span>
-        <span className={css.number}>+{this.props.data.number}</span>
-      </li>
-    );
-  }
+export default function Contact(props) {
+  return (
+    <li className={css.contactData}>
+      <span className={css.name}>{props.data.name}</span>
+      <span className={css.number}>+{props.data.number}</span>
+    </li>
+  );
 }
+
+Contact.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string,
+    id: PropTypes.string,
+  }),
+};
